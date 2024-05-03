94a8bd6ebf50c088b2bcc8f0f7e4ab5680ce3bf6
TABITHA <tabithaglele@MacBook-Pro-de-TABITHA.local>
2024-05-03 10:41:10 +0200

    login

--- src/AdminScreens/Login/LoginScreen.js
+++ src/AdminScreens/Login/LoginScreen.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './style.css';
 import { AUTH } from '../../firebaseConfig';
 import { useNavigate } from 'react-router-dom';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
@@ -27,38 +27,29 @@ const LoginScreen = () => {
     }
   };
  
-  const handleSignUp = async () => {
-   
-    try {
-      const response = await createUserWithEmailAndPassword(auth, email, password);
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-      alert('Sign up error: ' + error.message);
-    } finally {
-        handleNavigateParticipantForm()
-    }
-  };
+ 
 
   return (
     <div className='loginContainer'>
-      <h1>Login</h1>
-      <form>
-        <input
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+    <h1>Login</h1>
+    <form>
+      <input
+        className='inputField'
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
 
-        <input type="password"
-          placeholder="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </form>
-      <button onClick={handleSignIn}>Connexion</button>
-      <button onClick={handleSignUp}>Créer un compte</button>
-    </div>
+      <input
+        className='inputField'
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+    </form>
+    <button className='loginButton' onClick={handleSignIn}>Connexion</button>
+  </div>
   );
 };
 
